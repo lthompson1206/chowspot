4ca3e3528c42504651cd0eca2ebd1d02a7fb009e
Lamont <lthompson1206@gmail.com>
2019-11-17 10:45:27 -0500

    upload chowspot

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Chow$pot</h1>
+      <a target="_blank" rel="noopener noreferrer" href="http://www.recipepuppy.com/about/api/">
+      
+
+        Type in any food of your desire to find the recipe to... 
+      </a>
+    </div>
+  );
+}
+
+export default Jumbotron;
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="/">
+        Chow$pot
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
